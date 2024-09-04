b713349886f21d03d4aa4f2882b26d7a41c78548
Omprakash Kumar <npoint443@gmail.com>
2024-09-04 11:30:15 +0530

    this is the for each loop or enhanced loop

---
+++ For_Each.java
@@ -0,0 +1,9 @@
+public class For_Each {
+    public static void main(String[] args) {
+        int arr[]= { 3,4,5,6};
+        for(int n : arr)
+        {
+            System.out.print(n + " ");
+        }
+    }
+}
