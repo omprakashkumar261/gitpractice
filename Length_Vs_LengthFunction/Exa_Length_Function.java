7b100cafdd5ab2c33c8367e4d59339aded97d14d
Omprakash Kumar <npoint443@gmail.com>
2024-09-05 11:03:51 +0530

    length vs length()

---
+++ Length_Vs_LengthFunction/Exa_Length_Function.java
@@ -0,0 +1,8 @@
+package Length_Vs_LengthFunction;
+
+public class Exa_Length_Function {
+    public static void main(String[] args) {
+        String name ="Omprakash Kumar";
+        System.out.println(name.length());
+    }
+}
---
+++ Length_Vs_LengthFunction/Example.java
@@ -0,0 +1,26 @@
+package Length_Vs_LengthFunction;
+
+public class Example {
+    public static void main(String[] args) {
+        int arr[]= { 7,8,9,4};
+        System.out.println(arr.length);
+
+        /*example of 2 d array  */
+        // int nums[][]={
+        //                 {6,8,9,8},
+        //                 {8,7,9,5},
+        //                 {9,7,3,5}
+        //             };
+        // System.out.println(nums.length);
+
+        int nums[][]={
+                            {6,8,9,8},
+                            {8,7,9,5},
+                            {9,7,3,5}
+                    };
+            System.out.println(nums[0].length);
+            
+    
+
+    }
+}
