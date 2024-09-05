564bca0a72427fe75364de66a95e358a2d06b0c3
Omprakash Kumar <npoint443@gmail.com>
2024-09-05 12:11:57 +0530

    exception

---
+++ Avoid_Exception/AvoidException.java
@@ -0,0 +1,29 @@
+package Avoid_Exception;
+
+public class AvoidException {
+    public static void main(String[] args) {
+        // int arr[]={4,5,6};
+        // System.out.println(arr[0]);
+        // System.out.println("Bye");
+
+        /*write another example of the exception  */
+        //to find the length of the array 
+        // int arr[]={4,5,6};
+        // System.out.println(arr.length-1);
+        // System.out.println("Baye");
+
+        // find the exception by using the for loop 
+        // int in[]={7,8,9,4};
+        // for(int i=0; i<in.length; i++)
+        // {
+        //     System.out.print(in[i]+" ");
+        // }
+
+        // for enhanced loop 
+        int  arr[]={6,8,6,5};
+        for(int a:arr)
+        {
+            System.out.print(a +" ");
+        }
+    }
+}
